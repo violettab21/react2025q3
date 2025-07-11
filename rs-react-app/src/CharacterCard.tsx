c4f3d996f15f53b4fafb9f9ce313d09a8ddc637a
violettab21 <violettabatsura@gmail.com>
2025-07-12 00:43:50 +0300

    feat: fetch characters from API when page is opened

---
+++ rs-react-app/src/CharacterCard.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import './search.css';
+import type { Character } from './types';
+import './characterCard.css';
+
+export class CharacterCard extends Component<{ character: Character }> {
+  render() {
+    return (
+      <div className="cardContainer">
+        <div className="characterImageContainer">
+          <img
+            className="characterImage"
+            src={this.props.character.image}
+            alt="character image"
+          ></img>
+        </div>
+        <div className="characterInfo">
+          <p>Name: {this.props.character.name}</p>
+          <p>Gender: {this.props.character.gender}</p>
+          <p>Species: {this.props.character.species}</p>
+        </div>
+      </div>
+    );
+  }
+}
--- rs-react-app/src/Results.tsx
+++ rs-react-app/src/Results.tsx
@@ -1,15 +1,29 @@
 import { Component } from 'react';
 import './results.css';
+import type { CharactersResponse, Character } from './types';
+import { CharacterCard } from './CharacterCard';
+
+const url = 'https://rickandmortyapi.com/api/character?page=1&limit=30';
 
 export class Results extends Component {
+  state: { results: Character[] } = {
+    results: [],
+  };
+
+  async componentDidMount(): Promise<void> {
+    const response = await fetch(url);
+    if (response.ok) {
+      const resposeData: CharactersResponse = await response.json();
+      this.setState({ results: resposeData.results });
+    }
+  }
+
   render() {
     return (
       <div className="resultsContainer">
-        <div className="resultHeader">
-          <p className="columnName">Name</p>
-          <p className="columnDescription">Description</p>
-        </div>
-        <div className="resultRows"></div>
+        {this.state.results.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
       </div>
     );
   }
