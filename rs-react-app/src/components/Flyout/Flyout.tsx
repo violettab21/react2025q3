a58d0aef9e4abd4bdbe75b294b790100d1728e78
violettab21 <violettabatsura@gmail.com>
2025-08-02 01:41:35 +0300

    feat: implement file download

--- rs-react-app/src/components/Flyout/Flyout.tsx
+++ rs-react-app/src/components/Flyout/Flyout.tsx
@@ -3,6 +3,7 @@ import {
   useAppDispatch,
   useAppSelector,
 } from '../../store/store';
+import type { Character } from '../../types';
 import './flyout.css';
 
 export const Flyout = () => {
@@ -12,6 +13,27 @@ export const Flyout = () => {
   if (selectedCharacters.length === 0) {
     return null;
   }
+
+  const transformObject = (character: Character) => {
+    return {
+      name: character.name,
+      gender: character.gender,
+      location: character.location.name,
+      origin: character.origin.name,
+    };
+  };
+
+  const prepareFile = () => {
+    const header = `name, gender, location, origin\n`;
+    const contentString = selectedCharacters
+      .map((el) => transformObject(el))
+      .map((element) => Object.values(element))
+      .join('\n');
+    const finalString = `${header}${contentString}`;
+    const file = new Blob([finalString], { type: 'text/csv' });
+    const url = URL.createObjectURL(file);
+    return url;
+  };
 
   return (
     <div className="flyout">
@@ -23,7 +45,9 @@ export const Flyout = () => {
       >
         Unselect all
       </button>
-      <button>Download</button>
+      <a href={prepareFile()} download={`${selectedCharacters.length}_items`}>
+        Download
+      </a>
     </div>
   );
 };
